c0c01c6cb71a09982928d4b2197ee96f1687e77c
JAGPREET SINGH <singhjagpreet096@gmail.com>
2023-12-26 04:34:36 -0500

    file processing implemented

--- src/utils.py
+++ src/utils.py
@@ -3,20 +3,19 @@ import click
 from langchain.document_loaders import TextLoader
 from langchain.document_loaders import PyPDFLoader
 from langchain.vectorstores import Chroma
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+import chainlit as cl
 
 
 from src.config import Config
 import logging
 
-from dotenv import load_dotenv
-
-load_dotenv()
-
-
-
+text_splitter = RecursiveCharacterTextSplitter()
+embeddings = OpenAIEmbeddings()
 
 def process_file(file: AskFileResponse):
-    import tempfile 
+    import tempfile
 
     if file.type == "text/plain":
         Loader = TextLoader
@@ -27,52 +26,37 @@ def process_file(file: AskFileResponse):
         tempfile.write(file.content)
         loader = Loader(tempfile.name)
         documents = loader.load()
-        # text_splitter = text_splitter()
-        docs = Config.text_splitter.split_documents(documents)
-
+        docs = text_splitter.split_documents(documents)
         for i, doc in enumerate(docs):
             doc.metadata["source"] = f"source_{i}"
         return docs
 
-def get_docSearch(file,cl):
+def get_docsearch(file: AskFileResponse):
     docs = process_file(file)
 
-    logging.info("files loaded ")
+    # Save data in the user session
+    cl.user_session.set("docs", docs)
 
-    ## save data in user session 
-    cl.user_session.set("docs",docs)
+    # Create a unique namespace for the file
 
-    logging.info("docs saved in active session")
-    
-    docsearch = Chroma.from_documents(docs, Config.embeddings)
-
-    logging.info(f"embedding completed {type(Config.embeddings)}")
-
-    logging.info(f"type of docsearch {type(docsearch)}")
-
+    docsearch = Chroma.from_documents(
+        docs, embeddings
+    )
     return docsearch
 
-def get_source(sources,all_sources,docs,cl):
-    answer = []
-    source_elements = []
-    if sources:
-        found_sources = []
+def get_source(answer,source_documents):
+        text_elements = []
+        if source_documents:
+            for source_idx, source_doc in enumerate(source_documents):
+                source_name = f"source_{source_idx}"
 
-        # Add the sources to the message
-        for source in sources.split(","):
-            source_name = source.strip().replace(".", "")
-            # Get the index of the source
-            try:
-                index = all_sources.index(source_name)
-            except ValueError:
-                continue
-            text = docs[index].page_content
-            found_sources.append(source_name)
-            # Create the text element referenced in the message
-            source_elements.append(cl.Text(content=text, name=source_name))
+                text_elements.append(
+                    cl.Text(content=source_doc.page_content, name=source_name)
+                )
+            source_names = [text_el.name for text_el in text_elements]
 
-        if found_sources:
-            answer += f"\nSources: {', '.join(found_sources)}"
-        else:
-            answer += "\nNo sources found"
-    return source_elements,answer
+            if source_names:
+                answer += f"\nSources: {', '.join(source_names)}"
+            else:
+                answer += "\nNo source found"
+        return text_elements
